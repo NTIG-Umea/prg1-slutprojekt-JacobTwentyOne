2690b6b91f4603eafbcc48d7c1df3167e6f322c4
JacobTwentyOne <94972570+JacobTwentyOne@users.noreply.github.com>
2023-05-09 15:06:09 +0200

    Fixat en "template" men inte klar, den gör fortfarande paint

--- src/Slutprojekt.java
+++ src/Slutprojekt.java
@@ -1,2 +1,167 @@
-public class Slutprojekt {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+
+public class Slutprojekt extends Canvas implements Runnable{
+    private BufferStrategy bs;
+
+    private int mouseX = 0;
+    private boolean running = false;
+    private Thread thread;
+
+    private int pressX;
+    private int pressY;
+
+    private boolean isTimeToClear;
+
+
+    public Slutprojekt() {
+        setSize(600,400);
+        JFrame frame = new JFrame();
+        frame.add(this);
+        this.addKeyListener(new MyKeyListener());
+        this.addMouseMotionListener(new MyMouseMotionListener());
+        this.addMouseListener(new MyMouseListener());
+        requestFocus();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void render() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        // Rita ut den nya bilden
+        draw(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public void draw(Graphics g) {
+        if (isTimeToClear == true) {
+            g.clearRect(10,20,getWidth(),getHeight());
+            isTimeToClear = false;
+        }
+
+        g.fillOval (pressX, pressY, 5,5 );
+
+
+
+
+    }
+
+    private void update() {
+    }
+
+    public static void main(String[] args) {
+        Slutprojekt minGrafik = new Slutprojekt();
+        minGrafik.start();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        double ns = 1000000000.0 / 600.0;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            while(delta >= 1) {
+                // Uppdatera koordinaterna
+                update();
+                // Rita ut bilden med updaterad data
+                render();
+                delta--;
+            }
+        }
+        stop();
+    }
+
+    public class MyMouseMotionListener implements MouseMotionListener {
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            pressX = e.getX();
+            pressY = e.getY();
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            int  mouseX = e.getX();
+            int mouseY = e.getY();
+
+
+        }
+    }
+
+    public class MyMouseListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+    }
+
+    public class MyKeyListener implements KeyListener {
+
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyChar() == 'c' )  {
+                isTimeToClear = true;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+        }
+
+    }
 }
